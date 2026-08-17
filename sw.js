a7b35ab0deffe28d9faf4c0a08a29e9c35d38d53
fasc251284-droid <fasc251284@gmail.com>
2026-08-16 23:19:12 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Cache del cascarón: la app abre sin conexión y los datos quedan en localStorage */
-const CACHE = "evalef-v92";
+const CACHE = "evalef-v93";
 const BASICOS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
